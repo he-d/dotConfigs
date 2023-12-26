353d0c3f614657e8da8a04f020f741aba85fb67b
Halit Emre Dilalan <hedilalan@gmail.com>
2023-12-26 21:20:54 +0100

    update installerLinux.sh - add nodejs

--- installerLinux.sh
+++ installerLinux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# =============== Downlaod Section===============
+# Update System
 sudo apt upgrade && sudo apt upgrade -y
 
 # GIT
@@ -11,10 +13,10 @@ sudo apt install -y curl
 # SNAP
 sudo apt install -y snap
 
-#WGET
+# WGET
 sudo apt install -y wget
 
-#GPG
+# GPG
 sudo apt install -y gpg
 
 # FOR VM ONLY
@@ -24,6 +26,12 @@ sudo apt install open-vm-tools open-vm-tools-desktop -y
 sudo add-apt-repository ppa:aslatter/ppa
 sudo apt update
 sudo apt install -y alacritty
+
+# KITTY 
+sudo apt install -y kitty
+
+# NEOFETCH 
+sudo apt install -y neofetch
 
 # AWESOME
 sudo apt install -y awesome
@@ -47,10 +55,16 @@ sudo apt install -y g++
 sudo apt install -y font-manager
 
 # Python3 Virtual Envionment
-sudo apt-get install python3-venv
+sudo apt install -y python3-venv
 
 # Ripgrep for searching (fo e.g. nvim-telescop search)
 sudo pat install ripgrep
+
+#Java
+sudo apt install -y openjdk-11-jre
+sudo apt install -y openjdk-11-jdk
+sudo apt install -y gradle
+sudo apt install -y maven
 
 # SUBLIME TEXT
 sudo apt install -y gnupg apt-transport-https ca-certificates software-properties-common
@@ -71,25 +85,64 @@ sudo apt install -y code
 # NEOVIM
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-#./nvim.appimage --appimage-extract
-#./squashfs-root/AppRun --version
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 # VIM
 sudo apt insatll -y vim
 
+#Node JS
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install -y nodejs
+
+
+# Tmux and TPM
+sudo apt install -y tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# ZSH and Oh-My ZSH
+sudo apt install -y zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# set ZSH as default shell
+chsh -s ($which zsh)
+
+# Set default terminal emulator
+sudo update-alternatives --config x-terminal-emulator
+
 # Update sudoedit to vim
 sudo update-alternatives --config editor
 
 
+echo "Finished installing programms, starting the 'theConquerer' setup"
 
-echo git clone "https://github.com/he-d/dotConfigs.git"  to 
+
+echo download git repo "https://github.com/he-d/dotConfigs.git"  to 
 echo desktop and copy the configurations
 
+echo "......."
+echo "......."
+echo "......."
+echo "......."
+echo "......."
+echo "......."
+echo "......."
+echo "......."
+echo "......."
 
+echo "To complete the configuration reboot the system!"
 
-echo 
-echo Finished Installing Programms!
-echo Reboot the system now!
+    read -p "Reboot now? [y]/[n]" yn
+    case $yn in 
+        [Yy]* ) echo "Rebooting now";;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+exit
 
