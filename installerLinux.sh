ffddff2df7acbeb3f0f710926b33d534a5a57d93
Halit Emre Dilalan <hedilalan@gmail.com>
2023-12-27 22:29:52 +0100

    add packer installation for neovim

--- installerLinux.sh
+++ installerLinux.sh
@@ -90,6 +90,10 @@ chmod u+x nvim.appimage
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
+#Packer for NEOVIM
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 # VIM
 sudo apt insatll -y vim
 
